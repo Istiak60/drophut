c5c0bea18bc7f2cf2a4764557b8c21e23c84a321
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2022-02-09 23:21:11 +0600

    Checkout_page

---
+++ resources/views/frontend/pages/checkout/checkout.blade.php
@@ -0,0 +1,6 @@
+@extends('frontend.layouts.app')
+@section('content')
+    @include('frontend.pages.checkout.sections.breadcrumb')
+    @include('frontend.pages.checkout.sections.checkout-section')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('about','Frontend\AboutController@index');
 Route::get('blog-details','Frontend\BlogDetailController@index');
 Route::get('blog','Frontend\BlogController@index');
 Route::get('cart','Frontend\CartController@index');
+Route::get('checkout','Frontend\CheckoutController@index');
 
 Route::get('tracking','Frontend\TrackingController@index');
 Route::get('privacy-policy','Frontend\PrivacypolicyController@index');
