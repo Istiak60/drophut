fa3b4a6fc0d31b157809dd7f95baeb0d7326ebd5
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2022-02-10 00:07:36 +0600

    Forget_Password_done

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('cart','Frontend\CartController@index');
 Route::get('checkout','Frontend\CheckoutController@index');
 Route::get('contact','Frontend\ContactController@index');
 Route::get('faq','Frontend\FAQController@index');
+Route::get('forget-password','Frontend\ForgetPasswordController@index');
 
 
 
